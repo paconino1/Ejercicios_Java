e1c334fc1a443d562d7d27eaf268bd56bb961908
paconino1 <francninocaba@gmail.com>
2026-06-07 18:17:35 +0200

    Add files via upload

---
+++ EjemploLayout4/src/logica/Principal.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logica;
+import gui.Formulario2;
+
+/**
+ *
+ * @author Jesús Pérez
+ */
+public class Principal {
+    public static void main(String[] args) {
+        Formulario2 window=new Formulario2();
+        window.setVisible(true);
+        window.setLocationRelativeTo(null);
+    }
+}
